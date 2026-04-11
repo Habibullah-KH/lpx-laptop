49d59078e3a8aec8ba6b3f84da6330a34caf2254
Habib <habibullahkhandokar01@gmail.com>
2026-04-11 22:56:33 +0600

    performance section added

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,24 @@ import ProductsView from './component/ProductsView'
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
 import Showcase from './component/Showcase';
+import Performance from './component/Performance';
+import Features from './component/Features';
+import Highlight from './component/Highlight';
+import Footer from './component/Footer';
 
 gsap.registerPlugin(ScrollTrigger)
 const App = () => {
   return (
-    <>
+    <main>
     <Nav/>
     <Hero/>
     <ProductsView/>
     <Showcase/>
-    </>
+    <Performance/>
+    <Features/>
+    <Highlight/>
+    <Footer/>
+    </main>
   )
 }
 
